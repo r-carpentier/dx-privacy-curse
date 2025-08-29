cba4e4f4d54dcbfeda0587a39c4fb7bfb8c6bf22
Robin Carpentier <robin.carpentier@mq.edu.au>
2025-08-29 13:46:25 +1000

    Code formatting

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,12 @@
-from .dx import sample_noise_vectors, noisy_embeddings_to_ids_cp_chunked, noisy_embeddings_to_ids
-from .tools import best_uint_type, compute_distances_cp_chunked, compute_distances, rank_neighbors, argsort_chunked
+from .dx import (
+    sample_noise_vectors,
+    noisy_embeddings_to_ids_cp_chunked,
+    noisy_embeddings_to_ids,
+)
+from .tools import (
+    best_uint_type,
+    compute_distances_cp_chunked,
+    compute_distances,
+    rank_neighbors,
+    argsort_chunked,
+)
